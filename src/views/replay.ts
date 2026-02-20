03a8bcb154d76be70e6e3bed45e8b3c2521390d0
Austin Nhung <austinnhung@yahoo.ca>
2026-02-20 03:03:36 -0700

    Draw grid of map

--- src/views/replay.ts
+++ src/views/replay.ts
@@ -21,10 +21,10 @@ function setup() {
   }
 
   info(`Loading replay ${replayId}`);
-  Alpine.store("replay", { replayId: replayId });
-  loadReplayFile(replayId);
-
+  Alpine.store("replayId", replayId);
   Alpine.start();
+
+  loadReplayFile(replayId);
 }
 
 async function loadReplayFile(replayId: string) {
@@ -37,7 +37,7 @@ async function loadReplayFile(replayId: string) {
   // const actions = actionDataParser.parseActionData();
 
   const mapId = turns[0].maps_id;
-  Alpine.store("replay", { mapId: mapId });
+  Alpine.store("mapId", mapId);
   info(`Loading map ${mapId}`);
   loadMapFile(mapId);
 }
@@ -55,7 +55,48 @@ async function loadMapFile(mapId: number) {
     if (mapData.err === undefined) {
       await saveFile(mapFilename, JSON.stringify(mapData));
     } else {
-      Alpine.store("replay", { "mapLoadingError": mapData.message });
+      Alpine.store("mapLoadingError", mapData.message);
+    }
+  }
+
+  const sizeX = mapData["Size X"];
+  const sizeY = mapData["Size Y"];
+  const cellSize = 32;
+
+  info(`Successfully loaded map ${mapId}`);
+  Alpine.store("mapName", mapData.Name);
+  Alpine.store("mapAuthor", mapData.Author);
+  Alpine.store("mapSize", { x: sizeX, y: sizeY });
+  Alpine.store("cellSize", cellSize);
+
+  const canvas = document.getElementById("map-grid") as HTMLCanvasElement;
+  const context = canvas.getContext("2d");
+
+  if (context) {
+    canvas.width = sizeX * cellSize;
+    canvas.height = sizeY * cellSize;
+
+    info(`Canvas has size x=${canvas.width}, y=${canvas.height}`);
+
+    context.fillStyle = "black";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    context.strokeStyle = "rgba(255, 255, 255, 0.4)";
+    context.lineWidth = 1;
+
+    for (let col = 0; col <= sizeX; col++) {
+      const x = col * cellSize;
+      context.beginPath();
+      context.moveTo(x, 0);
+      context.lineTo(x, canvas.height);
+      context.stroke();
+    }
+    for (let row = 0; row <= sizeY; row++) {
+      const y = row * cellSize;
+      context.beginPath();
+      context.moveTo(0, y);
+      context.lineTo(canvas.width, y);
+      context.stroke();
     }
   }
 }
